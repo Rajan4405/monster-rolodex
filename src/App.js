cfc797fde2b00b4dd959c126646224ed031ec75b
Rajan4405 <rajanshatma652@gmail.com>
2022-10-28 15:34:44 +0545

    complete monter relodex app

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
       monsters: [],
       searchField: ''
     }
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -18,6 +19,11 @@ class App extends Component {
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
@@ -25,9 +31,10 @@ class App extends Component {
     )
     return (
       <div className='App'>
+        <h1>Monster Rolodex</h1>
         <SearchBox
           placeholder='search monters'
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
